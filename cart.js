7b3b635302164b737255c0dd0270ad195e8c9d1b
dobrygg <Dobryg4@mail.ru>
2024-11-18 11:49:36 +0300

    18 11

--- cart.js
+++ cart.js
@@ -5,44 +5,38 @@ const cart = {
     totalPrice: 0,
     count: 0,
 
-    getTotalPrice: function getTotalPrice() {
+    getTotalPrice() {
         return this.totalPrice;
     },
 
-    calculateItemPrice: function calculateItemPrice() {
+    calculateItemPrice() {
         this.totalPrice = this.items.reduce((sum, item) => {
-            if (typeof item.price === 'number' && typeof item.quantity === 'number' && item.price >= 0 && item.quantity >= 0) {
-                return sum + item.price * item.quantity;
-            } else {
-                console.warn(`Invalid item data: ${JSON.stringify(item)}. Skipping.`);
-                return sum;
-            }
+            
+            return sum + item.price * item.quantity;
+            
         }, 0);
     },
 
-    increaseCount: function increaseCount(num) {
+    increaseCount(num) {
         this.count += num; 
     },
 
-    add: function add(name, price, quantity = 1) { // Исправлено: quantity
-        if (typeof price !== 'number' || price < 0 || typeof quantity !== 'number' || quantity < 0) {
-            console.log(`Item not added.`);
-            return;
-        }
+    add(name, price, quantity = 1) { 
         const newItem = { name, price, quantity };
         this.items.push(newItem);
         this.calculateItemPrice(); 
         this.increaseCount(quantity); 
     },
 
-    clear: function clear() {
+    clear() {
         this.items = []; 
         this.totalPrice = 0; 
         this.count = 0; 
     },
 
-    print: function print() {
+    print() {
         console.log(JSON.stringify(this.items));
         console.log(this.totalPrice);
     },
 };
+
