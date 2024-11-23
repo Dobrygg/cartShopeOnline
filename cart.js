8a971278e157e6a4f920c32e47e8fc2b1385b126
dobrygg <Dobryg4@mail.ru>
2024-11-23 15:01:11 +0300

    23 11

--- cart.js
+++ cart.js
@@ -1,56 +1,43 @@
 'use strict'
 
+
 const cart = {
-    items: [],
-    _totalPrice: 0,
-    count: 0,
+items: [],
+get totalPrice() {
+return calculateltemPrice();
+},
+count: 0,
+calculateltemPrice() {
+    return this.items.reduce((sum, item) => {
+    return sum +  item.price * item.quantity;
+}, 0);
+},
+add(name, price, quantity = 1) {
+    const newItems = {
+    name ,
+    price ,
+    quantity
+}
+    this.items.push(newItems);
+    this.increaseCount(quantity);
+},
+increaseCount(num) {
+    this.count += num;
+},
+clear() {
+    this.items = [],
+    this.totalPrice = 0,
+    this.count = 0
+},
+print() {
+    console.log(JSON.stringify(this.items));
+}
 
-    calculateItemPrice() {
-        this._totalPrice = this.items.reduce((sum, item) => {
-            
-            return  sum + item.price * item.quantity;
-            
-
-        }, 0);
-    },
-
-    increaseCount(num) {
-        this.count += num; 
-    },
-
-    add(name, price, quantity = 1) { 
-        const newItem = { name, price, quantity };
-        this.items.push(newItem); 
-        this.increaseCount(quantity);
-        this.calculateItemPrice(); 
-    },
-
-    clear() {
-        this.items = []; 
-        this._totalPrice = 0; 
-        this.count = 0; 
-    },
-
-    print() {
-        console.log(JSON.stringify(this.items));
-        console.log( this._totalPrice);
-    },
 };
 
-Object.defineProperty(cart, 'totalPrice', {
-    get() {
-        this.calculateItemPrice();
-        return this._totalPrice;
-    }
-});
-
-
-cart.add('app', 16, 4);
-cart.add('qiwi', 3, 66);
-cart.add('banan', 22, 34);
-console.log(cart);
+cart.add('banan', 5, 2);
+cart.add('mango', 4, 1);
+cart.add('qiwi', 1, 10);
 cart.print();
 
-
-
-
+console.log(cart.calculateltemPrice());
