cfc496d5fc4d246068e5f1fb430fcd85417aa65d
dobrygg <Dobryg4@mail.ru>
2024-11-26 19:54:58 +0300

    установил eslint 26 11

--- cart.js
+++ cart.js
@@ -1,37 +1,36 @@
-'use strict'
+'use strict';
 
 
 const cart = {
-items: [],
-count: 0,
-get totalPrice() {
+  items: [],
+  count: 0,
+  calculateltemPrice() {
+    return this.items.reduce((sum, item) =>
+      sum + item.price * item.quantity, 0);
+  },
+  get totalPrice() {
     return calculateltemPrice();
-},
-calculateltemPrice() {
-    return this.items.reduce((sum, item) => {
-    return sum +  item.price * item.quantity;
-    }, 0);
-},
-add(name, price, quantity = 1) {
+  },
+  add(name, price, quantity = 1) {
     const newItems = {
-    name ,
-    price ,
-    quantity
-}
+      name,
+      price,
+      quantity,
+    };
     this.items.push(newItems);
     this.increaseCount(quantity);
-},
-increaseCount(num) {
+  },
+  increaseCount(num) {
     this.count += num;
-},
-clear() {
+  },
+  clear() {
     this.items = [],
     this.totalPrice = 0,
-    this.count = 0
-},
-print() {
+    this.count = 0;
+  },
+  print() {
     console.log(JSON.stringify(this.items));
-}
+  },
 };
 
 cart.add('banan', 5, 2);
